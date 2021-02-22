11fbee2372cf8b0197d397dda3cc95364712220a
mohmoniem281 <mohmoniem281@gmail.com>
2021-02-22 14:25:51 -0500

    Add files via upload

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
   author = 'YOUR NAME',                   # Type in your name
   author_email = 'your.email@domain.com',      # Type in your E-Mail
   url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/mohmoniem281/cornershop/archive/0.2.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/mohmoniem281/cornershop-catalog-integrations-api-tools/archive/11.1.tar.gz',    # I explain this later on
   keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'requests'
